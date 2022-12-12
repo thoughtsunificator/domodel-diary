c65b80adffe6ddf15f12092fa751375c0a5c905d
Romain Lebesle <oss@thoughtsunificator.me>
2022-12-12 02:12:58 +0100

    Pad first week with previous month' days

--- src/model/view/diary/day.js
+++ src/model/view/diary/day.js
@@ -1,5 +1,5 @@
 export default data => ({
 	tagName: "td",
-	className: `day ${data.date.getMonth() === new Date().getMonth() && data.date.getDate() === new Date().getDate() && data.date.getFullYear() === new Date().getFullYear() ? 'current' : '' }`,
+	className: `day${data.date.getMonth() === new Date().getMonth() && data.date.getDate() === new Date().getDate() && data.date.getFullYear() === new Date().getFullYear() ? ' current' : '' }${data.grayed ? " grayed" : ""}`,
 	textContent: data.date.getDate()
 })
